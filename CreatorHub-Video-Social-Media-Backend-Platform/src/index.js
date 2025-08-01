a5a1a74e820355083d6a3d218f8a47e04370005a
AshishDara <97780553+AshishDara@users.noreply.github.com>
2025-08-01 22:52:13 +0530

    Feat: Implement root entry point for Vercel deployment

--- CreatorHub-Video-Social-Media-Backend-Platform/src/index.js
+++ CreatorHub-Video-Social-Media-Backend-Platform/src/index.js
@@ -1,54 +1,18 @@
+
 import dotenv from 'dotenv'
 import connectToDB from './db/dbConnect.js'
 import app from './app.js'
 
 dotenv.config({
-    path: '../.env'
+    path: './.env'
 })
 
-// console.log("Connecting with MONGODB_URL:", process.env.MONGODB_URL);
 connectToDB()
 .then(() => {
-    // app.listen(process.env.PORT || 8000, () => {
-    //     console.log(`Server is listening on: ${process.env.PORT}`);
-    // })
-    // We don't need app.listen here for Vercel
-    console.log("MongoDB connected successfully. The app is ready.");
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
 })
-.catch((error) => console.log("MONGODB connection failed!!!: ", error))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**import express from 'express'
-const app = express()
-
-( async() => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("Error", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.log("Error", error);
-        throw error
-    }
-})()
-**/
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
